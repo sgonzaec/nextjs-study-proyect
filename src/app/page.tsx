ee4570658566762ff611dbf24fcbcfca88e8c03a
Simon Gonzalez <simongonzalezechavarria@gmail.com>
2024-03-22 22:53:20 +0100

    feat: base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <span className="text-5xl">Hola Simon!</span>
-      <Link href={'/about'}>About Page</Link>
     </main>
   );
 }
